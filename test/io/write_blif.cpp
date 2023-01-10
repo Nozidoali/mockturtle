7c9edb6bfd8c09952d47add8101efc64fa4ad840
nozidoali <nozidoali@gmail.com>
2023-01-10 01:44:42 +0100

    fix(blif): add testcases for PO&RI, but failed to reproduce the error

--- test/io/write_blif.cpp
+++ test/io/write_blif.cpp
@@ -38,6 +38,31 @@ void blif_read_after_write_test( const Ntk& to_write )
   }
 }
 
+template<class Ntk>
+void blif_multiple_definition_test( const Ntk& to_write )
+{
+  std::ostringstream out;
+  write_blif_params ps;
+  ps.skip_feedthrough = true;
+  write_blif( to_write, out, ps );
+
+  Ntk to_read;
+  std::istringstream in( out.str() );
+  auto const ret = lorina::read_blif( in, blif_reader( to_read ) );
+
+  CHECK( ret == lorina::return_code::success );
+
+  CHECK( to_read.size() == to_write.size() ); 
+  CHECK( to_read.num_pis() == to_write.num_pis() ); 
+  CHECK( to_read.num_pos() == to_write.num_pos() ); 
+  CHECK( to_read.num_gates() == to_write.num_gates() );
+
+  if constexpr ( has_num_registers_v<Ntk> )
+  {
+    CHECK( to_read.num_registers() == to_write.num_registers() ); 
+  }
+}
+
 TEST_CASE( "write a simple combinational k-LUT into BLIF file", "[write_blif]" )
 {
   klut_network klut;
@@ -202,7 +227,6 @@ TEST_CASE( "write a sequential k-LUT with name view", "[write_blif]" )
 
   std::ostringstream out;
   write_blif( klut, out );
-  write_blif( klut, "test.blif" );
 
   /* additional spaces at the end of .inputs and .outputs */
   CHECK( out.str() == ".model top\n"
@@ -263,7 +287,6 @@ TEST_CASE( "write a sequential k-LUT with name view and skip feed through", "[wr
   write_blif_params ps;
   ps.skip_feedthrough = true;
   write_blif( klut, out, ps );
-  write_blif( klut, "test.blif" );
 
   /* additional spaces at the end of .inputs and .outputs */
   CHECK( out.str() == ".model top\n"
@@ -283,6 +306,101 @@ TEST_CASE( "write a sequential k-LUT with name view and skip feed through", "[wr
                       "1-1 1\n"
                       "11- 1\n"
                       ".end\n" );
+
+  blif_read_after_write_test( klut );
+}
+
+TEST_CASE( "write a sequential k-LUT with a node driving both PO and RI (skip feedthrough = false)", "[write_blif]" )
+{
+  /**
+   * 
+   *              f4 (PO)
+   *             /  \
+   *            f2  f3
+   *             | /
+   *             f1 (RI & PO)
+   *            / | \
+   *           a  b  c
+   */
+  
+  names_view<sequential<klut_network>> klut;
+
+  const auto a = klut.create_pi( "a" );
+  const auto b = klut.create_pi( "b" );
+  const auto c = klut.create_pi( "c" );
+
+  const auto f1 = klut.create_maj( a, b, c );
+  const auto f2 = klut.create_ro(); // f2 <- f1
+  const auto f3 = klut.create_ro(); // f3 <- f1
+  const auto f4 = klut.create_xor( f2, f3 );
+
+  klut.set_name( f1, "maj(a,b,c)" );
+  klut.set_name( f2, "dff1" );
+  klut.set_name( f3, "dff2" );
+  klut.set_name( f4, "xor(dff1,dff2)" );
+
+  klut.create_po( f4, "output" );
+  klut.create_po( f1, "ri&po" );
+  klut.create_ri( f1 ); // f2 <- f1
+  klut.create_ri( f1 ); // f3 <- f1
+
+  CHECK( klut.num_gates() == 2 );
+  CHECK( klut.num_registers() == 2 );
+  CHECK( klut.num_pis() == 3 );
+  CHECK( klut.num_pos() == 2 );
+
+  std::ostringstream out;
+  write_blif_params ps;
+  ps.skip_feedthrough = false;
+  write_blif( klut, out, ps );
+
+  blif_read_after_write_test( klut );
+}
+
+
+TEST_CASE( "write a sequential k-LUT with a node driving both PO and RI (skip feedthrough = true)", "[write_blif]" )
+{
+  /**
+   * 
+   *              f4 (PO)
+   *             /  \
+   *            f2  f3
+   *             | /
+   *             f1 (RI & PO)
+   *            / | \
+   *           a  b  c
+   */
+  
+  names_view<sequential<klut_network>> klut;
+
+  const auto a = klut.create_pi( "a" );
+  const auto b = klut.create_pi( "b" );
+  const auto c = klut.create_pi( "c" );
+
+  const auto f1 = klut.create_maj( a, b, c );
+  const auto f2 = klut.create_ro(); // f2 <- f1
+  const auto f3 = klut.create_ro(); // f3 <- f1
+  const auto f4 = klut.create_xor( f2, f3 );
+
+  klut.set_name( f1, "maj(a,b,c)" );
+  klut.set_name( f2, "dff1" );
+  klut.set_name( f3, "dff2" );
+  klut.set_name( f4, "xor(dff1,dff2)" );
+
+  klut.create_po( f4, "output" );
+  klut.create_po( f1, "ri&po" );
+  klut.create_ri( f1 ); // f2 <- f1
+  klut.create_ri( f1 ); // f3 <- f1
+
+  CHECK( klut.num_gates() == 2 );
+  CHECK( klut.num_registers() == 2 );
+  CHECK( klut.num_pis() == 3 );
+  CHECK( klut.num_pos() == 2 );
+
+  std::ostringstream out;
+  write_blif_params ps;
+  ps.skip_feedthrough = true;
+  write_blif( klut, out, ps );
 
   blif_read_after_write_test( klut );
 }
